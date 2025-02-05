12ff69fca1abde7fba074666ecdfdd0bebdf607e
Nezune <kinight@gmail.com>
2025-02-04 22:57:27 -0300

    Update ublockjscript.js

--- ublockjscript.js
+++ ublockjscript.js
@@ -1,5 +1,4 @@
 /// youtube.js
 if(window.location.toString().match("/shorts/")){window.location.replace(window.location.toString().replace('/shorts/', '/watch?v='))}
 
-/// twitter.js
 if(window.location.toString().match("x.com/")){window.location.replace(window.location.toString().replace('x.com/', 'nitter.poast.org/'))}
